56fe8d5c92f1257cfbc0ab4cb9f03fc21de2acab
whiteiong <986247535@qq.com>
2020-07-21 18:37:06 +0800

    admin and url and views
    完成了后台，路由以及模板的教程

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
+from django.http import HttpResponse
 
-# Register your models here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.template import loader
 
-from django.http import HttpResponse
+from django.shortcuts import render # 快捷函数
+from django.http import Http404 #不存在这个模板
+from .models import Question
+from django.shortcuts import get_object_or_404, render
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:6]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
 
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+#
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+
+#
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+#
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
